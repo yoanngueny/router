3cc36be7c49c0959058f5f8785179c3a65bf505b
Willy Brauner <mail@willybrauner.com>
2021-03-19 17:33:27 +0100

    WIP test

---
+++ test/RouterInstance.test.ts
@@ -0,0 +1,7 @@
+describe("RouterInstance", () => {
+  it("should get routes from URL", () => {});
+
+  it("should emit current and previous route", () => {});
+
+  it("should emit current and previous route", () => {});
+});
